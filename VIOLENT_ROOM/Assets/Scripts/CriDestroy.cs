1938bb41f44dd1341c54a9ad60602a74d3483240
KonishiYuuki <kd1259128@st.kobedenshi.ac.jp>
2022-12-05 11:28:16 +0900

    サウンドの処理を実装

---
+++ VIOLENT_ROOM/Assets/Scripts/CriDestroy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using CriWare;
+
+// SEÇÃçƒê∂Ç™èIóπÇµÇΩÇÁé©ìÆÇ≈è¡Ç∑
+public class CriDestroy : MonoBehaviour
+{
+    CriAtomSource _source;
+
+    private void Awake()
+    {
+        _source = transform.GetComponent<CriAtomSource>();
+    }
+
+    void Update()
+    {
+        if (_source.status == CriAtomSource.Status.PlayEnd)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
